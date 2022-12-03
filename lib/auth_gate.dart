31f4914f4908a4180779abcda88f03b7534e6173
FelixJes <felixbelix007@gmail.com>
2022-12-03 10:44:37 +0530

    .

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
-import 'package:login_page/main.dart';
+import 'package:get/get.dart';
 import 'package:login_page/service/session.dart';
 
 class AuthGate extends StatelessWidget {
@@ -12,31 +12,34 @@ class AuthGate extends StatelessWidget {
     return StreamBuilder<User?>(
       stream: AppSession().firebaseAuth.authStateChanges(),
       builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          if (snapshot.hasData) {
-            return const MyHomePage(
-              title: "Logged In",
-            );
-          } else {
-            return SignInScreen(
-              styles: const {
-       
-              },
-              providerConfigs: const [
-                EmailProviderConfiguration(),
-              ],
-              headerBuilder: ((context, constraints, shrinkOffset) {
-                return Center(
-                  child: Text("Loign"),
-                );
-              }),
-            );
-          }
+       if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
         }
-        if (snapshot.hasError) {}
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
+        if (!snapshot.hasData) {
+          return const MaterialApp(
+              title: 'Flutter Demo',
+              home: SignInScreen(providerConfigs: [
+                EmailProviderConfiguration(),
+              ]));
+        } else {
+          return GetMaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              colorScheme: lightColorScheme,
+              primarySwatch: Colors.blue,
+              textTheme: TextTheme(
+                bodyText1: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 16),
+                bodyText2: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 13),
+                headline1: GoogleFonts.secularOne(color: Colors.black, fontSize: 35, fontWeight: FontWeight.w700),
+                headline3: const TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16),
+              ),
+            ),
+            home: Container(),
+            builder: ((context, child) {
+              return LandingPage(child: child ?? Container());
+            }),
+          );
+        }
       },
     );
   }
